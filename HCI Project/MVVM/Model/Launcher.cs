a9fb7751fbf278a8be1ec5c1fed4152b82b142ff
NerosJourney <sandman4264@gmail.com>
2022-11-09 20:22:39 -0500

    Created Launcher base class with abstract properties and methods

---
+++ HCI Project/MVVM/Model/Launcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCI_Project.MVVM.Model
+{
+    public enum LauncherID
+    {
+        Steam,
+        Epic
+    }
+    public abstract class Launcher
+    {
+        public abstract string Name { get; }
+        public abstract LauncherID id { get; }
+
+        public abstract bool LaunchGame();
+        public abstract Game[] FindGames();
+        public abstract void GetGameInfo(Game game);
+    }
+}
