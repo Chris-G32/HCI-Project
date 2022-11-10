e2db4d2573e61405e2d98c42df77c88490046f36
NerosJourney <sandman4264@gmail.com>
2022-11-09 20:44:36 -0500

    Created Steam Launcher class (not yet populated)

---
+++ HCI Project/MVVM/Model/Launcher_Steam.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Policy;
+using System.Text;
+
+namespace HCI_Project.MVVM.Model
+{
+    public class Launcher_Steam:Launcher
+    {
+        public Launcher_Steam()
+        {
+            _url = "";
+            _key = "";
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Steam";
+            }
+        }
+        public override LauncherID ID
+        {
+            get
+            {
+                return LauncherID.Steam;
+            }
+        }
+
+        public override bool LaunchGame(Game game)
+        {
+            Debug.WriteLine("Launching " + game.Name + "...");
+            return true;
+        }
+
+        public override Game[] FindGames()
+        {
+            Debug.WriteLine("Finding all games from Steam...");
+            return null;
+        }
+
+        public override void GetGameInfo(ref Game game)
+        {
+            Debug.WriteLine("Getting info about " + game.Name);
+        }
+    }
+}
