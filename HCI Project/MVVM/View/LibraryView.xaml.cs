04e6ba81f41b0a06ba3fda5cf30444136cf17882
Chris-G32 <cgruska@kent.edu>
2022-11-10 12:06:57 -0500

    Redid bindings and changed file paths along with how some visual elements work

--- HCI Project/MVVM/View/LibraryView.xaml.cs
+++ HCI Project/MVVM/View/LibraryView.xaml.cs
@@ -31,15 +31,15 @@ namespace HCI_Project.MVVM.View
         }
 
         //Used to Bind Library to a list of Games Ideally
-        public ObservableCollection<string> ListSource
-        {
-            get { return (ObservableCollection<string>)GetValue(ListSourceProperty); }
-            set { SetValue(ListSourceProperty, value); }
-        }
+        //public ObservableCollection<string> ListSource
+        //{
+        //    get { return (ObservableCollection<string>)GetValue(ListSourceProperty); }
+        //    set { SetValue(ListSourceProperty, value); }
+        //}
 
-        // Using a DependencyProperty as the backing store for ListSource.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ListSourceProperty =
-            DependencyProperty.Register("ListSource", typeof(ObservableCollection<string>), typeof(LibraryView), new PropertyMetadata(new ObservableCollection<string>()));
+        //// Using a DependencyProperty as the backing store for ListSource.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty ListSourceProperty =
+        //    DependencyProperty.Register("ListSource", typeof(ObservableCollection<string>), typeof(LibraryView), new PropertyMetadata(new ObservableCollection<string>()));
 
         private void LibraryItemSelect(object sender, MouseButtonEventArgs e)
         {
