422d62230da0d8bfcede46d64fa3358c8e8e4988
Chris-G32 <cgruska@kent.edu>
2022-11-10 10:39:15 -0500

    Changed to handle librarysubviews

---
+++ HCI Project/MVVM/ViewModel/LibraryViewModels/LibraryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using HCI_Project.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace HCI_Project.MVVM.ViewModel.LibraryViewModels
+{
+    public class LibraryViewModel : ObservableObject
+    {
+        //May End Up Not Being Stored Here, may make a copy to order list how they please possibly
+        public ObservableCollection<object> GamesList { get; private set; }
+        public RelayCommand OpenGamePage { get; set; }
+        public LibraryViewModel()
+        {
+            OpenGamePage = new RelayCommand(o =>
+              {
+                  //cast o to the type of GamesList elemeent
+
+              });
+        }
+
+    }
+}
