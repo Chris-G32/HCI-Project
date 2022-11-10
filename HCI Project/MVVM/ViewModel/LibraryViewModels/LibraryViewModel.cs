15032307414e29f4a9d6d7bc0375d07ec57df6e5
Chris-G32 <cgruska@kent.edu>
2022-11-10 12:07:24 -0500

    Library View Models now functioning as a submain view to handle all views which also have the library visible

--- HCI Project/MVVM/ViewModel/LibraryViewModels/LibraryViewModel.cs
+++ HCI Project/MVVM/ViewModel/LibraryViewModels/LibraryViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using HCI_Project.Core;
+using HCI_Project.MVVM.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
 
 namespace HCI_Project.MVVM.ViewModel.LibraryViewModels
@@ -9,15 +11,43 @@ namespace HCI_Project.MVVM.ViewModel.LibraryViewModels
     public class LibraryViewModel : ObservableObject
     {
         //May End Up Not Being Stored Here, may make a copy to order list how they please possibly
-        public ObservableCollection<object> GamesList { get; private set; }
+        // public ObservableCollection<object> GamesList { get; private set; }
+
+        //SubViews
+        public GameViewModel GameVM { get; set; }
+
+        //Relay Commands
         public RelayCommand OpenGamePage { get; set; }
+        public ObservableCollection<Game> OwnedGames { get; set; }
+        
+        private static object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
         public LibraryViewModel()
         {
+            //Initializing members
+            OwnedGames = new ObservableCollection<Game>() { new Game("Shrt"), new Game("Medium Len Title"), new Game("A particularly very long title of a game that is still going on") };
+            GameVM = new GameViewModel();
+
+            //Set Default View
+            CurrentView = GameVM;
+            
+            //Set Relay Commands
             OpenGamePage = new RelayCommand(o =>
               {
-                  //cast o to the type of GamesList elemeent
+                  //Expecting to recieve a game object as parameter, also a int referring to its location in list should work
+                  GameVM = new GameViewModel((Game)o);
 
               });
+            
         }
 
     }
