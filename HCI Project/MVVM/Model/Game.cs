2ecbb281c09b8be7317efc183111fde703993383
Chris-G32 <cgruska@kent.edu>
2022-11-10 12:04:12 -0500

    Changed Game to be a class

--- HCI Project/MVVM/Model/Game.cs
+++ HCI Project/MVVM/Model/Game.cs
@@ -10,10 +10,28 @@ namespace HCI_Project.MVVM.Model
         INSTALLED,
         NOT_OWNED
     }
-    public struct Game
+    public class Game
     {
-        public int Game_ID { get; set; }
-        public string Name { get; set; }
+        public Game(int game_ID, string name, LauncherID launcher_ID, string[] tags, string[] images, string description, GameState state)
+        {
+            Game_ID = game_ID;
+            Name = name;
+            Launcher_ID = launcher_ID;
+            Tags = tags;
+            Images = images;
+            Description = description;
+            State = state;
+        }
+
+        /// <summary>
+        /// For testing Purposes
+        /// </summary>
+        public Game(string name)
+        {
+            Name=name;
+        }
+        public int Game_ID { get; }
+        public string Name { get; }
         // Contains a value from the LauncherID enum from Launcher.cs
         public LauncherID Launcher_ID { get; set; }
         public String[] Tags { get; set; }
