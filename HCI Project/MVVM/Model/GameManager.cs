a563d60e1841996dc868c7a40cb1b5b8c8871e40
NerosJourney <sandman4264@gmail.com>
2022-11-14 21:03:39 -0500

    Created Game Manager class to handle multiple launchers

---
+++ HCI Project/MVVM/Model/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCI_Project.MVVM.Model
+{
+    public class GameManager
+    {
+        public List<Game> games { get; set; }
+    }
+}
