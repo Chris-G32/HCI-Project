5e9a02b0454d39a97f5192adc62d52e3640b4600
NerosJourney <sandman4264@gmail.com>
2022-11-14 21:15:16 -0500

    Added LaunchGame and FindAllGames methods to GameManager

--- HCI Project/MVVM/Model/GameManager.cs
+++ HCI Project/MVVM/Model/GameManager.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HCI_Project.MVVM.Model
 {
     public class GameManager
     {
         public List<Game> games { get; set; }
+
+        private Launcher_Steam _steamLauncher;
+
+        public GameManager()
+        {
+            _steamLauncher = new Launcher_Steam();
+        }
+
+        /// <summary>
+        /// Launches any game using the proper launcher
+        /// </summary>
+        public void LaunchGame(Game game)
+        {
+            if (game.Launcher_ID == LauncherID.Steam)
+                _steamLauncher.LaunchGame(game);
+            else
+                Debug.WriteLine("Game Launcher not yet implemented...");
+                
+        }
+
+        /// <summary>
+        /// Finds games on all currently configured launchers
+        /// </summary>
+        public async Task FindAllGames()
+        {
+            List<Game> steamGames = await _steamLauncher.FindGames();
+            foreach (Game game in steamGames)
+            {
+                games.Add(game);
+            }
+        }
     }
 }
