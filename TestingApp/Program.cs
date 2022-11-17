364d5d2611bb4f24ef3af20c31f1e86ce2f17865
NerosJourney <sandman4264@gmail.com>
2022-11-16 23:33:33 -0500

    Created a test database using dummy data to verify SQLite

--- TestingApp/Program.cs
+++ TestingApp/Program.cs
@@ -1,19 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace TestingApp
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        //static async Task Main(string[] args)
+        //{
+        //    Launcher_Steam steam = new Launcher_Steam();
+        //    List<Game> games = await steam.FindGames();
+        //    foreach(Game game in games)
+        //    {
+        //        Console.WriteLine(game.Name + ": " + game.Game_ID);
+        //    }
+        //}
+
+        static void Main(string[] args)
         {
-            Launcher_Steam steam = new Launcher_Steam();
-            List<Game> games = await steam.FindGames();
-            foreach(Game game in games)
-            {
-                Console.WriteLine(game.Name + ": " + game.Game_ID);
-            }
+            string cs = @"URI=file:../../../test.db";
+
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+
+            using var cmd = new SQLiteCommand(con);
+
+            cmd.CommandText = "DROP TABLE IF EXISTS cars";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE cars(id INTEGER PRIMARY KEY,
+            name TEXT, price INT)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Audi',52642)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Mercedes',57127)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Skoda',9000)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Volvo',29000)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Bentley',350000)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Citroen',21000)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Hummer',41400)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Volkswagen',21600)";
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Table cars created");
         }
     }
 }
