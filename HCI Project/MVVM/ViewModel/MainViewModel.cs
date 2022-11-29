f5bed6ccc29dc631ca3ac246f0f371928b99d5cd
ChrisG-32 <cgruska@kent.edu>
2022-11-28 22:50:05 -0500

    Dropped some commented stuff

--- HCI Project/MVVM/ViewModel/MainViewModel.cs
+++ HCI Project/MVVM/ViewModel/MainViewModel.cs
@@ -31,21 +31,7 @@ namespace HCI_Project.MVVM.ViewModel
             set { 
                 //Set Value
                 _searchFor = value;
-                //Clear Displayed Items
-                //if (_searchFor == null || _searchFor== ""){
-                //    SearchList=new ObservableCollection<Game>(LibraryVM.OwnedGames);
-                //}
-                //else {
-                //    SearchList.Clear();
-                //    //Query database for results
-                //    foreach(var game in LibraryVM.OwnedGames)
-                //    {
-                //        if ((game.Name.ToLower()).Contains(_searchFor.ToLower()))
-                //        {
-                //           SearchList.Add(game);
-                //        }
-                //    } 
-                //}
+                //Returns all games meeting query
                 SearchList = GameHandler.SearchByName(_searchFor);
                 OnPropertyChanged();
             } 
