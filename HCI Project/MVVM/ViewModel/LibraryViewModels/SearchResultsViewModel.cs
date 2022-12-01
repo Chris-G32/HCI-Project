874d4d9bf3781ff0c600a69e787418c8caf642d1
ChrisG-32 <cgruska@kent.edu>
2022-11-30 22:22:57 -0500

    Created search res page and vm

---
+++ HCI Project/MVVM/ViewModel/LibraryViewModels/SearchResultsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using HCI_Project.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCI_Project.MVVM.ViewModel.LibraryViewModels
+{
+    public class SearchResultsViewModel:ObservableObject
+    {
+        public SearchResultsViewModel() { } 
+    }
+}
