069789d9e1b71d678a6709476ec0064980462447
ChrisG-32 <cgruska@kent.edu>
2022-12-02 01:18:39 -0500

    Homeview functions properly almost fully, needs a highlighton hover still

--- HCI Project/MVVM/ViewModel/LibraryViewModels/HomeViewModel.cs
+++ HCI Project/MVVM/ViewModel/LibraryViewModels/HomeViewModel.cs
@@ -17,12 +17,12 @@ namespace HCI_Project.MVVM.ViewModel.LibraryViewModels
         public BitmapImage Imgtst { get; set; }=null;
         private Uri _testUri=null;
         public Uri TestUri { get { return _testUri; } set { _testUri = value; OnPropertyChanged(); } }
-       
+        public LibraryViewModel Parent { get; set; }
         public ObservableCollection<Game> RecentlyPlayed { get; }
         public Game testbind { get; set; }
         //Temporary test command to update image of something
         public RelayCommand PlayGame { get; set; }
-        public HomeViewModel()
+        public HomeViewModel(LibraryViewModel parent = null)
         {
             const int MAXRECENTLYPLAYED= 6;
             RecentlyPlayed = new ObservableCollection<Game>();
@@ -39,7 +39,7 @@ namespace HCI_Project.MVVM.ViewModel.LibraryViewModels
             {
                 MainViewModel.GameHandler.LaunchGame(o as Game);
             });
-
+            Parent = parent;
         }
     }
 }
