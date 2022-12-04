5eacd19f8e1092c8357e6a0016c737513f2b8ce0
ChrisG-32 <cgruska@kent.edu>
2022-12-04 14:25:16 -0500

    Non Fully functional seach bar being implemented still but some nice changes made

---
+++ HCI Project/MVVM/View/LibraryViews/ImageResources/Custom Controls/SearchBar.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI_Project.MVVM.View.LibraryViews.ImageResources.Custom_Controls
+{
+    /// <summary>
+    /// Interaction logic for SearchBar.xaml
+    /// </summary>
+    public partial class SearchBar : UserControl
+    {
+        public SearchBar()
+        {
+            InitializeComponent();
+        }
+
+
+        public ListCollectionView SearchResults
+        {
+            get { return (ListCollectionView)GetValue(SearchResultsProperty); }
+            set { SetValue(SearchResultsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SearchResults.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SearchResultsProperty =
+            DependencyProperty.Register("SearchResults", typeof(ListCollectionView), typeof(SearchBar));
+
+
+
+        public string SearchText
+        {
+            get { return (string)GetValue(SearchTextProperty); }
+            set { SetValue(SearchTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SearchText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SearchTextProperty =
+            DependencyProperty.Register("SearchText", typeof(string), typeof(SearchBar), new PropertyMetadata("Search"));
+
+
+
+    }
+}
--- HCI Project/MVVM/ViewModel/LibraryViewModels/SearchResultsViewModel.cs
+++ HCI Project/MVVM/ViewModel/LibraryViewModels/SearchResultsViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using HCI_Project.Core;
+using HCI_Project.MVVM.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Policy;
 using System.Text;
+using System.Windows.Data;
 
 namespace HCI_Project.MVVM.ViewModel.LibraryViewModels
 {
     public class SearchResultsViewModel:ObservableObject
     {
-        public SearchResultsViewModel() { } 
+        public string Query { get; set; }
+        public ListCollectionView Results { get; set; }
+        private bool FilterByName(object filterMe)
+        {
+            //BEGIN OF LOGIC FOR NICER SEARCH
+            var game = filterMe as Game;
+            return (game.Name.ToUpper()).Contains(Query.ToUpper());
+        }
+        public SearchResultsViewModel(string query) {
+            Query= query;
+            Results = new ListCollectionView(MainViewModel.GameHandler.Games);
+            Results.Filter = FilterByName;
+        } 
     }
 }
