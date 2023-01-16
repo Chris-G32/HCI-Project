852e54a489c6ecbb819b9bf1760c0ce7aeb9b9ce
ChrisG-32 <cgruska@kent.edu>
2023-01-16 16:12:09 -0500

    Finished the core folder comment and restructure

--- HCI Project/Core/ObservableObject.cs
+++ HCI Project/Core/ObservableObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//DONE WITH RESTRUCTURE AND COMMENT
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -6,6 +7,10 @@ using System.Text;
 
 namespace HCI_Project.Core
 {
+    /// <summary>
+    /// Inherit from this to implement <see cref="System.ComponentModel.INotifyPropertyChanged"/> on a class.
+    /// NOTE: Classes that need to inherit from more than one class need to implement the interface seperately
+    /// </summary>
     public class ObservableObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
--- HCI Project/Core/RelayCommand.cs
+++ HCI Project/Core/RelayCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//DONE WITH RESTRUCTURE AND COMMENT
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 namespace HCI_Project.Core
 {
+    /// <summary>
+    /// Implements the <see cref="ICommand"/> to allow for easy binding of simple commands 
+    /// </summary>
     public class RelayCommand : ICommand
     {
         private Action<object> _execute;
@@ -25,6 +30,11 @@ namespace HCI_Project.Core
             remove { CommandManager.RequerySuggested -= value; }
         }
 
+        /// <summary>
+        /// Creates a command to be bound to in XAML
+        /// </summary>
+        /// <param name="execute">Procedure to run, must return null. A lambda expression is generally used.</param>
+        /// <param name="canExecute">A predicate function that determines if execute can be executed</param>
         public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
             _execute = execute;
